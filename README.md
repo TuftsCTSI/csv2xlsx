64c0314af2ecc8e209855d2502609456d163096f
Clark C. Evans <cce@clarkevans.com>
2024-01-24 05:36:40 -0500

    initial bootstrap via maven template

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple binary that takes a CSV file and a password and produces an xlsx file.
---
+++ src/main/java/org/tuftsctsi/CSV2XLSX.java
@@ -0,0 +1,13 @@
+package org.tuftsctsi;
+
+/**
+ * Hello world!
+ *
+ */
+public class CSV2XLSX
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
